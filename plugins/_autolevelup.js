646f5b01e6148bbdb0879939f9b93b4ebbe7c074
mamoutj <141942894+mamoutj@users.noreply.github.com>
2023-08-13 17:20:12 +0100

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,5 +1,5 @@
 //import db from '../lib/database.js'
-import { canLevelUp } from '../lib/levelling.js'
+export { canLevelUp } from '../lib/levelling.js'
 
 export async function before(m, { conn }) {
     let user = global.db.data.users[m.sender]
